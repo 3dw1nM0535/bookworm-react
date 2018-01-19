0eabcfd1366c43345541e7e70b8a678e7bc01e4a
3dw1nM0535 <workockmoses@gmail.com>
2018-01-19 12:35:17 +0300

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-const App = () => <h1>Welcome to Bookworm platform</h1>
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+
+const App = () => (
+  <div>
+    <Route path='/' exact component={HomePage} />
+    <Route path='/login' exact component={LoginPage} />
+  </div>
+)
 
 export default App;
