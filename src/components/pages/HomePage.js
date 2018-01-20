039c3202858ee0b69545f8ebbd7f69eeba25a6e0
3dw1nM0535 <workockmoses@gmail.com>
2018-01-20 13:23:52 +0300

    Implemented user login/logout

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Button } from 'semantic-ui-react';
+import * as actions from '../../actions/auth';
 
-const HomePage = () => (
+const HomePage = ({ isAuthenticated, logout }) => (
   <div>
     <h1>Welcome to HomePage of Bookworm platform</h1>
-    <Link to='/login'>Login</Link>
+    { isAuthenticated ? <Button primary onClick={() => logout()}>Logout</Button> : <Link to='/login'>Login</Link> }
   </div>
 );
 
-export default HomePage;
+HomePage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired
+}
+
+function mapStateToProps (state) {
+  return {
+    isAuthenticated: !!state.user.token
+  };
+}
+
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
