ed0afeffd651ad4027b4247477b00654a8f67547
3dw1nM0535 <workockmoses@gmail.com>
2018-02-01 15:14:06 +0300

    implemented Smart UI for authenticated and unauthenticated user dashboard, add new book, search book in book collections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
@@ -9,11 +10,14 @@ import DashboardPage from './components/pages/DashboardPage';
 import ConfirmationPage from './components/pages/ConfirmationPage';
 import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
 import ResetPasswordPage from './components/pages/ResetPasswordPage';
+import AddNewBookPage from './components/pages/AddNewBookPage';
+import TopNavigation from './components/navigation/TopNavigation';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
 
-const App = ({ location }) => (
+const App = ({ location, isAuthenticated }) => (
   <div className='ui container'>
+    {isAuthenticated && <TopNavigation /> }
     <Route location={location} path='/' exact component={HomePage} />
     <Route location={location} path='/confirmation/:token' exact component={ConfirmationPage} />
     <GuestRoute location={location} path='/signup' exact component={SignupPage} />
@@ -21,13 +25,20 @@ const App = ({ location }) => (
     <GuestRoute location={location} path='/reset_password' component={ForgotPasswordPage} />
     <GuestRoute location={location} path='/reset-password/:token' component={ResetPasswordPage} />
     <UserRoute location={location} path='/dashboard' exact component={DashboardPage} />
+    <UserRoute location={location} path='/books/new' exact component={AddNewBookPage} />
   </div>
 )
 
 App.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    isAuthenticated: !!state.user.email
+  };
+}
+export default connect(mapStateToProps)(App);
--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -3,20 +3,30 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 
 import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
+import { allBooksSelector } from '../../reducers/books';
+import AddBook from '../AddBook';
 
-const DashboardPage =({ isConfirmed }) => (
+const DashboardPage =({ isConfirmed, books }) => (
   <div>
     { !isConfirmed && <ConfirmEmailMessage /> }
+
+    { books.length === 0 && <AddBook /> }
   </div>
 );
 
 DashboardPage.propTypes = {
-  isConfirmed: PropTypes.bool.isRequired
+  isConfirmed: PropTypes.bool.isRequired,
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
 };
 
 function mapStateToProps (state) {
   return {
-    isConfirmed: !!state.user.confirmed
+    isConfirmed: !!state.user.confirmed,
+    books: allBooksSelector(state)
   };
 }
 
