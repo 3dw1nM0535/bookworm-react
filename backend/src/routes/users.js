2d27b9f52cba7e18f02e81d21f5110ca2d127b1e
3dw1nM0535 <workockmoses@gmail.com>
2019-02-26 13:51:03 +0300

    fetch current logged in user

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from '../models/users';
 import parseErrors from '../utils/parseErrors';
 import { sendConfirmationEmail } from '../mailer';
+import authenticate from "../middlewares/authenticate";
 
 const router = express.Router();
 
@@ -15,6 +16,16 @@ router.post('/', (req, res) => {
       sendConfirmationEmail(userRecord);
       res.json({ user: userRecord.toAuthJSON() })})
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
+router.get("/current_user", authenticate, (req, res) => {
+  res.json({
+    user: {
+      email: req.currentUser.email,
+      confirmed: req.currentUser.confirmed,
+      username: req.currentUser.username
+    }
+  });
 });
 
 export default router;
