745ff98bb9997967520a24d04fb0174b0ef89b78
3dw1nM0535 <workockmoses@gmail.com>
2019-02-28 09:55:25 +0300

    glamorous component on app init

--- src/components/pages/NewCharacterPage.js
+++ src/components/pages/NewCharacterPage.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { slideInLeft } from "react-animations";
+import * as glamor from "glamor";
+import glamorous from "glamorous";
+
+const FadeInDiv = glamorous.div({
+  animation: `1s ${glamor.css.keyframes(slideInLeft)}`
+});
 
 class NewCharacterPage extends React.Component {
   state = {
@@ -9,27 +16,59 @@ class NewCharacterPage extends React.Component {
     return (
       <div>
         <nav className="nav nav-pills nav-justified">
-          <button className={`nav-item nav-link ${this.state.step === 1 ? "active" : ""}`}>
+          <button className={`nav-item nav-link ${this.state.step === 1 ? "active" : ""}`} onClick={
+            () => this.setState({ step: 1 })
+          }>
             <h4>Step 1</h4>
             <p>Race</p>
           </button>
-          <button disabled className="nav-link nav-item">
+          <button className={`nav-link nav-item ${this.state.step === 2 ? "active" : ""}`} onClick={
+            () => this.setState({ step: 2 })
+          }>
             <h4>Step 2</h4>
             <p>Class</p>
           </button>
-          <button disabled className="nav-link nav-item">
+          <button className={`nav-link nav-item ${this.state.step === 3 ? "active" : ""}`} onClick={
+            () => this.setState({ step: 3 })
+          }>
             <h4>Step 3</h4>
             <p>Background</p>
           </button>
-          <button disabled className="nav-link nav-item">
+          <button className={`nav-link nav-item ${this.state.step === 4 ? "active" : ""}`} onClick={
+            () => this.setState({ step: 4 })
+          }>
             <h4>Step 4</h4>
             <p>Faction</p>
           </button>
-          <button disabled className="nav-item nav-link">
+          <button className={`nav-link nav-item ${this.state.step === 5 ? "active" : ""}`} onClick={
+            () => this.setState({ step: 5 })
+          }>
             <h4>Step 5</h4>
             <p>Name</p>
           </button>
         </nav>
+        <div>
+          {this.state.step === 1 &&
+            <FadeInDiv>
+              <h2>STEP 1</h2>
+            </FadeInDiv>}
+          {this.state.step === 2 &&
+            <FadeInDiv>
+              <h2>STEP 2</h2>
+            </FadeInDiv>}
+          {this.state.step === 3 &&
+            <FadeInDiv>
+              <h2>STEP 3</h2>
+            </FadeInDiv>}
+          {this.state.step === 4 &&
+            <FadeInDiv>
+              <h2>STEP 4</h2>
+            </FadeInDiv>}
+            {this.state.step === 5 &&
+              <FadeInDiv>
+                <h2>STEP 5</h2>
+              </FadeInDiv>}
+        </div>
       </div>
     )
   }
